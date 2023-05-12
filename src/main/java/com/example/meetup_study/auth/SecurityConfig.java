710c42cfd7072aa653b7ac02e7aa6e7f08942ebc
doi-h <jeonghwanlee15@gmail.com>
2023-05-12 09:40:31 +0900

    spring security config 추가

---
+++ src/main/java/com/example/meetup_study/auth/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.example.meetup_study.auth;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+@Slf4j
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        log.debug("[SecurityConfig] configure()");
+
+        http
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .csrf().disable()
+                .formLogin().disable()
+                .httpBasic().disable();
+    }
+}
