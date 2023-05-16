3888d3f2f5dbf7e0bbe4d9e03c741e13cd73da79
doi-h <jeonghwanlee15@gmail.com>
2023-05-16 20:12:00 +0900

    aop 커스텀 어노테이션 및 메소드 실행속도 측정 기능 추가

---
+++ src/main/java/com/example/meetup_study/common/aop/TimerAop.java
@@ -0,0 +1,38 @@
+package com.example.meetup_study.common.aop;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+
+@Slf4j
+@Aspect
+@Component
+public class TimerAop {
+
+    @Pointcut("@annotation(com.example.meetup_study.common.aop.Timer)")
+    private void timer(){}
+
+    @Around("timer()")
+    public void timer(ProceedingJoinPoint joinPoint) throws Throwable {
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+
+        joinPoint.proceed();
+
+        stopWatch.stop();
+
+        long totalTimeMillis = stopWatch.getTotalTimeMillis();
+
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        String methodName = signature.getMethod().getName();
+
+        log.info("실행 메서드: {}, 실행시간 = {}ms", methodName, totalTimeMillis);
+    }
+
+}
