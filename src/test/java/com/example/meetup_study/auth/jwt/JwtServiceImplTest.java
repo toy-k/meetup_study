c8cf817fac349e2578d92045232ce3f8d46fb312
doi-h <jeonghwanlee15@gmail.com>
2023-05-26 16:52:40 +0900

    git action test4

--- src/test/java/com/example/meetup_study/auth/jwt/JwtServiceImplTest.java
+++ src/test/java/com/example/meetup_study/auth/jwt/JwtServiceImplTest.java
@@ -21,7 +21,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+@SpringBootTest(classes = JwtServiceImpl.class)
 @AutoConfigureMockMvc
 @DisplayName("JwtServiceImpl")
 class JwtServiceImplTest {
