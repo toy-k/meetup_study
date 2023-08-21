69797a76f977dbbfd43dc360c8afcccb6362da74
doi-h <jeonghwanlee15@gmail.com>
2023-08-21 17:39:26 +0900

    docker mysql replication 구현중

--- docker/db/mysql/init/init.sql
+++ docker/db/mysql/init/init.sql
@@ -7,3 +7,11 @@
 -- ${INIT_SQL_USER}@'%';
 
 CREATE DATABASE meetup_study_DB DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+grant all privileges on meetup_study_DB.* to 'user'@'%' identified by '1234';
+
+grant replication slave on *.* to 'user'@'%';
+
+flush privileges;
+
+CREATE DATABASE meetup_slave_DB DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
